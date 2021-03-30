4a81e3cc19cee5bc6a5fb1d707a52179f1305584
nguyensontung140293 <nguyensontung1402@gmail.com>
2021-03-30 11:17:45 +0700

    netcore

---
+++ ef01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;z
+
+namespace ef01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
